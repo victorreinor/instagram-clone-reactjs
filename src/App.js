2ded5008a0fa84394a21d8e23958de009b0eda8e
Victor Reinor <victorreinor@gmail.com>
2019-06-12 13:00:10 -0300

    importa o componente header para exibir na aplicacao

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './global.css'
+
+import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   );
