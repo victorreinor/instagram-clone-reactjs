84d2462d5f13a8a62de640eb58be4c44c7f31b0b
Victor Reinor <victorreinor@gmail.com>
2019-06-13 20:50:44 -0300

    adiciona a estrutura que sera feita o feed de noticias e sua estilizacao

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 
 import './Feed.css';
 
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 class Feed extends Component {
   render() {
     return (
-      <h1>Feed</h1>
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Victor Reinor</span>
+              <span className="place">Mirassol</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img src="http://localhost:3333/files/cachorro_macho_1.jpg" alt="" />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="" />
+              <img src={comment} alt="" />
+              <img src={send} alt="" />
+            </div>
+
+            <strong>900 curtidas</strong>
+            <p>
+              Um post muito massa.
+              <span>#react #omnistack #top</span>
+            </p>
+          </footer>
+        </article>
+      </section>
     );
   }
 }
